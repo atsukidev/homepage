9cba40daf9f438263901a81ec2587e07e96983c7
Brenno Fagundes <dev.whiteflag@gmail.com>
2020-09-23 19:41:23 -0300

    feat: added hero image rotate on hover (#2)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 $(document).ready(function () {
+
+	// Floating image on Hero
+	const wrapper = $(".floating-element-wrapper");
+	wrapper.on('mousemove', function (e) {
+		const x = e.clientX - $(this).offset().left + $(window).scrollLeft();
+		const y = e.clientY - $(this).offset().top + $(window).scrollTop();
+
+		// Relative XY to card
+		const rY = map(x, 0, $(this).width(), -10, 10);
+		const rX = map(y, 0, $(this).height(), -10, 10)
+
+		$(this).children(".floating-element")
+			.css("transform", "rotateY(" + rY + "deg)" + " " + "rotateX(" + -rX + "deg)");
+	});
+
+	wrapper.on('mouseenter', function () {
+		$(this).children(".floating-element").css({
+			transition: "all " + 0.8 + "s" + " linear"
+		});
+	});
+
+	wrapper.on('mouseleave', function () {
+		$(this).children(".floating-element").css({
+			transition: "all " + 0.8 + "s" + " linear"
+		});
+		$(this).children(".floating-element")
+			.css("transform", "rotateY(" + 0 + "deg)" + " " + "rotateX(" + 0 + "deg)");
+	});
+
+	// Slick Slideshow in Tech We Use
 	$('#tech-logos').slick({
 		slidesToShow: 4,
 		slidesToScroll: 1,
@@ -22,3 +52,7 @@ $(document).ready(function () {
 		}]
 	});
 });
+
+function map(direction, in_min, in_max, out_min, out_max) {
+	return (direction - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
